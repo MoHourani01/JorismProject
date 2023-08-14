258bf2dcf232f635a20a5c2f3934d79e48728488
MoHourani01 <m.t.hourani01@gmail.com>
2023-08-14 21:47:50 +0300

    date time picker solved

--- jorism_project/lib/cubit/cubit.dart
+++ jorism_project/lib/cubit/cubit.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -9,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:jorism_project/cubit/state.dart';
+import 'package:jorism_project/models/products_model.dart';
 import 'package:jorism_project/screens/Agencies/Agencies_Screen.dart';
 import 'package:jorism_project/screens/Profile/Profile.dart';
 import 'package:jorism_project/screens/home/home_screen.dart';
@@ -16,15 +16,15 @@ import 'package:jorism_project/screens/news/news_screen.dart';
 import 'package:jorism_project/screens/places/places_screen.dart';
 import 'package:path/path.dart' as path;
 
-class JorismCubit extends Cubit<JorismState>{
-  JorismCubit(): super(InitialJorismState());
+class JorismCubit extends Cubit<JorismState> {
+  JorismCubit() : super(InitialJorismState());
 
-  static JorismCubit get(BuildContext context){
+  static JorismCubit get(BuildContext context) {
     return BlocProvider.of(context);
   }
 
-  int currentIndex=0;
-  List<Widget> screens=[
+  int currentIndex = 0;
+  List<Widget> screens = [
     HomeScreen(),
     NewsScreen(),
     AgenciesScreen(),
@@ -32,20 +32,69 @@ class JorismCubit extends Cubit<JorismState>{
     ProfileScreen(),
   ];
 
-  void changeIndex(int index){
-    currentIndex=index;
+  void changeIndex(int index) {
+    currentIndex = index;
     emit(JorismChangeBottomNavBarState());
   }
 
-  int activatedIndex = 0;
-  int activatedAgencyIndex = 0;
+  // int activatedIndex = 0;
+  // int activatedAgencyIndex = 0;
 
-  Future<void> datePicker(BuildContext context)async{
-    await showDatePicker(
+  DateTime dateTime=DateTime.now();
+  DateTime selectedDate = DateTime.now();
+  String formattedSelectedDate = '';
+  Future<void> datePicker(BuildContext context) async {
+    final pickDate = await showDatePicker(
       context: context,
       initialDate: DateTime.now(),
       firstDate: DateTime.now(),
       lastDate: DateTime(2030),
-    );
+
+    ).then((value) {
+      selectedDate=value!;
+      print(selectedDate);
+    });
+  }
+
+
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  ProductsModel? productsModel;
+
+  // Future<void> addProducts(ProductsModel product, String userId) async {
+  //   emit(AddProductLoadingState());
+  //   await FirebaseFirestore.instance
+  //       .collection('products')
+  //       .doc(product.productId)
+  //       .collection('userProducts')
+  //       .doc(userId)
+  //       .set(product.toProductsJson()).then((value) {
+  //         emit(AddProductSuccessState());
+  //   })
+  //       .catchError((error) {
+  //         emit(AddProductErrorState(error.toString()));
+  //   });
+  //   // firestore
+  //   //     .collection('products')
+  //   //     .add(product.toProductsJson())
+  //   //     .then((value) {
+  //   //       emit(AddProductSuccessState() as JorismState);
+  //   // })
+  //   //     .catchError((error) {
+  //   //       emit(AddProductErrorState(error.toString()) as JorismState);
+  //   // });
+  // }
+
+  Future<void> addProduct(ProductsModel product) async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    await firestore.collection('products').doc(product.productId).set(product.toProductsJson())
+        .whenComplete(() {
+          emit(AddProductSuccessState());
+      print('Product added successfully');
+    }).catchError((error) {
+      emit(AddProductErrorState(error.toString()));
+      // print('Error adding product: $error');
+    });
   }
 }
